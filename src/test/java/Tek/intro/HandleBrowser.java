ed89a71f8fdb09622e0bb4def8006db20127f00b
Altaf <safi.mohammad.altaf@gmail.com>
2024-07-09 20:22:42 -0500

    spartan selenium project

--- src/test/java/Tek/intro/HandleBrowser.java
+++ src/test/java/Tek/intro/HandleBrowser.java
@@ -5,10 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class HandleBrowser {
     public static void main(String[] args) {
         ChromeDriver chromeDriver = new ChromeDriver();
-        ChromeDriver chromeDriver1 = new ChromeDriver();
+
 
         chromeDriver.get("http://google.com");
-        chromeDriver1.get("http://facebook.com");
+
 
 
 
