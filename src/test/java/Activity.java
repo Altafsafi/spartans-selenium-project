aee94940ab733f3c1f4cd6ae85490784b22e7720
Altaf <safi.mohammad.altaf@gmail.com>
2024-07-09 20:44:21 -0500

    spartan selenium project

---
+++ src/test/java/Activity.java
@@ -0,0 +1,19 @@
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity {
+
+    public static void main(String[] args) {
+
+
+        ChromeDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get("https://dev.insurance.tekschool-students.com/");
+
+       String title = chromeDriver.getTitle();
+        System.out.println(title);
+
+        chromeDriver.quit();
+
+    }
+}
--- src/test/java/Tek/intro/HandleBrowser.java
+++ src/test/java/Tek/intro/HandleBrowser.java
@@ -6,8 +6,16 @@ public class HandleBrowser {
     public static void main(String[] args) {
         ChromeDriver chromeDriver = new ChromeDriver();
 
-
+        // Navigate to Chrome browser
         chromeDriver.get("http://google.com");
+        //To maximize the window size
+        chromeDriver.manage().window().maximize();
+
+       String title  =   chromeDriver.getTitle();
+        System.out.println(title);
+
+        //To close the page
+        chromeDriver.quit();
 
 
 
