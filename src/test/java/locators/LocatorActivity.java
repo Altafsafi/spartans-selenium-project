2a6cc6fa83c02662b621a7b93d1bdeb11c51eb78
Altaf <safi.mohammad.altaf@gmail.com>
2024-07-10 20:44:28 -0500

    spartan selenium project

--- src/test/java/locators/LocatorActivity.java
+++ src/test/java/locators/LocatorActivity.java
@@ -11,20 +11,19 @@ public class LocatorActivity {
         WebDriver webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         webDriver.get("https://retail.tekschool-students.com/");
-
+ //1
      By NavigateToLogin = By.id("signinLink");
-
      WebElement findTheLogin = webDriver.findElement(NavigateToLogin);
         findTheLogin.click();
-
+//2
        By Email = By.id("email");
        WebElement Emailbox = webDriver.findElement(Email);
        Emailbox.sendKeys("lordaltaf@gmail.com");
-
+//3
       By Password = By.id("password");
       WebElement Enterpassword = webDriver.findElement(Password);
       Enterpassword.sendKeys("Kabul123");
-
+//4
      By Login = By.id("loginBtn");
      WebElement Click = webDriver.findElement(Login);
      Click.click();
